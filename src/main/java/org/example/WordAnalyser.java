e4a94342da363276ca2b72575b44cf1e734e9ada
AbiPetheram <abi.gill97@gmail.com>
2024-07-29 15:28:01 +0100

    feat: add WordAnalyser class

---
+++ src/main/java/org/example/WordAnalyser.java
@@ -0,0 +1,25 @@
+package org.example;
+import java.util.Map;
+
+public class WordAnalyser {
+    public String[] findLongestWords(String text) {
+        // TODO: Implement the logic to find the longest word(s) in the given text
+        //separate words
+        String[] words = text.replaceAll("[^a-zA-Z ]", "").split("\\s+");
+        //loop through each word to get length
+        int index = 0;
+
+        for (int i = 0; i < words.length; i++){
+            if (words[i].length() > words[index].length()){
+                index = i;
+            }
+        }
+        //get the word with longest length
+        return new String[]{words[index]};
+    }
+
+    public Map<Character, Integer> calculateLetterFrequency(String text) {
+        // TODO: Implement the logic to calculate the frequency of each letter in the given text
+        return null;
+    }
+}
---
+++ src/test/java/org/example/WordAnalyserTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordAnalyserTest {
+    WordAnalyser wordAnalyser;
+
+    @BeforeEach
+    public void setUp() {
+        wordAnalyser = new WordAnalyser();
+    }
+
+    @Test
+    @DisplayName("Test returns the longest word")
+    void testFindLongestWord() {
+        // TODO: Implement the logic to find the longest word(s) in the given text
+        String[] result = wordAnalyser.findLongestWords("This is a fairly boring sentence.");
+        String[] expected = {"sentence"};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void testCalculateLetterFrequency() {
+        // TODO: Implement the logic to calculate the frequency of each letter in the given text
+    }
+}
